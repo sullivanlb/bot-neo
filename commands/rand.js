c9efb8e4a748f0ce2ccb70045ecbc3c9c677d0e5
sullivan_lb <sullivan.lb56@gmail.com>
2021-02-01 23:12:05 +0100

    random features improved

--- commands/rand.js
+++ commands/rand.js
@@ -1,10 +1,10 @@
 module.exports = (client, message, args) => {
 
     if(args[0] != undefined && args[1] != undefined){
-        let rand = Math.floor(Math.random() * (args[1] - args[0]) + args[0])
+        let rand = Math.floor(Math.random() * ((parseInt(args[1])+1) - args[0]) + args[0])
         message.channel.send("Le nombre est : " + rand)
     }
     else{
-        message.channel.send("Donne moi deux arguments, un nombre minimum et un nombre maximum (ex : `.rand 0 10` --> chiffre aléatoire entre 0 et 10)")
+        message.channel.send("Donne moi deux arguments, un nombre minimum et un nombre maximum (ex : `.rand 0 10` --> chiffre aléatoire entre 0 inclus et 10 inclus)")
     }
 }
