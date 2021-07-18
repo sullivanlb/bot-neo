35b910ce5ad44a1181d2c9492ba320363b62d06c
sullivan_lb <sullivan.lb56@gmail.com>
2021-07-18 21:20:44 +0200

    addind a port

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ const Discord = require("discord.js");
 const { Collection } = require("discord.js");
 const client = new Discord.Client();
 require("dotenv").config();
+const express = require('express')
+const app = express()
 
 // client.PREFIX = config.PREFIX;
 client.PREFIX = process.env.PREFIX;
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App is running on port ${ PORT }`);
+});
 
 client.commands = new Collection();
 client.commands.set("afk", require("./commands/afk.js"));
