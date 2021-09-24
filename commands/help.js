b8bdc6a7a928e2a188c6ad5dd45cbf9b5f0fcdfe
sullivan_lb <sullivan.lb56@gmail.com>
2021-09-24 12:56:13 +0200

    help command updated

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,8 @@ module.exports = (client, message) => {
         .addField("Commandes", "- `status` <status> : affiche [<status>] à gauche de ton pseudo (`.status help` pour plus d'infos)\r\n" + 
                                 "- `tg` <mention> : Je vais faire taire la personne que tu mentionnes\r\n" + 
                                 "- `rand` <min> <max> : Je vais te retourner un nombre entre [ min ; max ]\r\n" + 
-                                "- `roulette` <help|join|clear|queue|start> : On joue à la roulette russe et la personne choisie est kick du serveur")
+                                "- `roulette` <help|join|clear|queue|start> : On joue à la roulette russe et la personne choisie est kick du serveur" + 
+                                "- `.spam` <mention> <number>: le robot va spammer la personne mentionnée un nombre donné de fois")
         .setTimestamp()
         .setFooter("By Scipio");
                 
